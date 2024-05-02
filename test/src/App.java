901af7e76813ca280ad3a2d9b2ed3a55bb88bda7
YuJin Choi <cheauj@gmail.com>
2024-05-02 23:46:02 +0900

    first commit

---
+++ test/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("'-----------'\n");
+        System.out.printf("|%s          |\n", "A");
+        System.out.printf("|           |\n");
+        System.out.printf("|           |\n");
+        System.out.printf("|           |\n");
+        System.out.printf("|     %s     |\n", "♥");
+        System.out.printf("|           |\n");
+        System.out.printf("|           |\n");
+        System.out.printf("|           |\n");
+        System.out.printf("|          %s|\n", "A");
+        System.out.printf("'-----------'\n");
+
+        
+
+        System.out.printf("'-----------'    '-----------'\n");
+        System.out.printf("|%s          |    |%s          |\n", "A", "A");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|     %s     |    |     %s     |\n", "♥", "♥");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|          %s|    |          %s|\n", "A", "A");
+        System.out.printf("'-----------'    '-----------'\n");
+
+
+
+    }
+}
