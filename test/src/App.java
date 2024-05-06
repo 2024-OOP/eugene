f8c01b249259ee39774638d28b65fa6657d989de
YuJin Choi <cheauj@gmail.com>
2024-05-06 20:33:50 +0900

    1장 ~ 5장 덱카드 드로우 시 출력하는 함수들 구성

--- test/src/App.java
+++ test/src/App.java
@@ -1,32 +1,97 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void printOne(String[] patterns, String[] numbers)
+    {
         System.out.printf("'-----------'\n");
-        System.out.printf("|%s          |\n", "A");
+        System.out.printf("|%s          |\n", numbers[0]);
         System.out.printf("|           |\n");
         System.out.printf("|           |\n");
         System.out.printf("|           |\n");
-        System.out.printf("|     %s     |\n", "♥");
+        System.out.printf("|     %s     |\n", patterns[0]);
         System.out.printf("|           |\n");
         System.out.printf("|           |\n");
         System.out.printf("|           |\n");
-        System.out.printf("|          %s|\n", "A");
+        System.out.printf("|          %s|\n", numbers[0]);
         System.out.printf("'-----------'\n");
-
+    }
+    
+    public static void printTwo(String[] patterns, String[] numbers)
+    {
+        System.out.printf("'-----------'    '-----------'\n");
+        System.out.printf("|%s          |    |%s          |\n", numbers[0], numbers[1]);
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|     %s     |    |     %s     |\n", patterns[0], patterns[1]);
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|           |    |           |\n");
+        System.out.printf("|          %s|    |          %s|\n", numbers[0], numbers[1]);
+        System.out.printf("'-----------'    '-----------'\n");
+    }
+    
+    public static void printThree(String[] patterns, String[] numbers)
+    {
+        System.out.printf("'-----------'    '-----------'    '-----------'\n");
+        System.out.printf("|%s          |    |%s          |    |%s          |\n", numbers[0], numbers[1], numbers[2]);
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|     %s     |    |     %s     |    |     %s     |\n", patterns[0], patterns[1], patterns[2]);
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |\n");
+        System.out.printf("|          %s|    |          %s|    |          %s|\n", numbers[0], numbers[1], numbers[2]);
+        System.out.printf("'-----------'    '-----------'    '-----------'\n");
+    }
+    
+    public static void printFour(String[] patterns, String[] numbers)
+    {
+        System.out.printf("'-----------'    '-----------'    '-----------'    '-----------'\n");
+        System.out.printf("|%s          |    |%s          |    |%s          |    |%s          |\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|     %s     |    |     %s     |    |     %s     |    |     %s     |\n", patterns[0], patterns[1], patterns[2], patterns[3]);
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |\n");
+        System.out.printf("|          %s|    |          %s|    |          %s|    |          %s|\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+        System.out.printf("'-----------'    '-----------'    '-----------'    '-----------'\n");
+    }
+    
+    public static void printFive(String[] patterns, String[] numbers)
+    {
+        System.out.printf("'-----------'    '-----------'    '-----------'    '-----------'    '-----------'\n");
+        System.out.printf("|%s          |    |%s          |    |%s          |    |%s          |    |%s          |\n", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|     %s     |    |     %s     |    |     %s     |    |     %s     |    |     %s     |\n", patterns[0], patterns[1], patterns[2], patterns[3], patterns[4]);
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|           |    |           |    |           |    |           |    |           |\n");
+        System.out.printf("|          %s|    |          %s|    |          %s|    |          %s|    |          %s|\n", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+        System.out.printf("'-----------'    '-----------'    '-----------'    '-----------'    '-----------'\n");
         
+    }
 
-        System.out.printf("'-----------'    '-----------'\n");
-        System.out.printf("|%s          |    |%s          |\n", "A", "A");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|     %s     |    |     %s     |\n", "♥", "♥");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|           |    |           |\n");
-        System.out.printf("|          %s|    |          %s|\n", "A", "A");
-        System.out.printf("'-----------'    '-----------'\n");
+    public static void clearConsole() {
+        // ANSI 이스케이프 시퀀스를 사용하여 콘솔을 지웁니다.
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    
+    public static void main(String[] args) throws Exception {
 
+        String[] numbers = {"A", "2", "3", "4", "5"};
+        String[] patterns = {"◈", "♥", "♣️", "♥", "♠️"};
 
+        printOne(patterns, numbers);
+        clearConsole();
+        printTwo(patterns, numbers);
 
+        // printThree(patterns, numbers);
+        // printFour(patterns, numbers);
+        // printFive(patterns, numbers);
     }
 }
---
+++ test/src/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+printFour(patterns, numbers);
+        printFive(patterns, numbers);
